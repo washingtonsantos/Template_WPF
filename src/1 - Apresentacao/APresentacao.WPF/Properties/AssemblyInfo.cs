c6f85b3edbb98f054145aa401f6ccf3ed45d51f6
washington.santos <washingtonsilvasan@gmail.com>
2020-03-18 09:10:24 -0300

    Update namespace APresentacao to Apresentacao

--- src/1 - Apresentacao/APresentacao.WPF/Properties/AssemblyInfo.cs
+++ src/1 - Apresentacao/APresentacao.WPF/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("APresentacao.WPF")]
+[assembly: AssemblyTitle("Apresentacao.WPF")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("APresentacao.WPF")]
+[assembly: AssemblyProduct("Apresentacao.WPF")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- src/1 - Apresentacao/APresentacao.WPF/Properties/Resources.Designer.cs
+++ src/1 - Apresentacao/APresentacao.WPF/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Apesentacao.WPF.Properties {
+namespace Apresentacao.WPF.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Apesentacao.WPF.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Apesentacao.WPF.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Apresentacao.WPF.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
